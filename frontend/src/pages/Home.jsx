56e3600a50f6840608ed9a1f2293b24d4785d4da
prashig <168104409+prashig@users.noreply.github.com>
2026-03-09 22:32:47 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/hero.jsx'
+import Hero from '../components/Hero.jsx'
 import Category from '../components/Category.jsx'
 import Pros from '../components/Pros.jsx'
 import Footer from '../components/Footer.jsx'
